21f959ff159fc10160c00cf9ae31c05ce73cb0c5
patrikduch <duchpatrik@icloud.com>
2019-03-13 11:46:18 +0100

    Routing extension

--- ReactApp/Frontend/Routes.tsx
+++ ReactApp/Frontend/Routes.tsx
@@ -7,25 +7,28 @@
 //-----------------------------------------------------------------------
 import * as React from 'react';
 import HomeComp from './components/Home';
-import CustomerPage from './pages/CustomerPage';
-import OrderPage from './pages/OrderPage';
-import ProductPage from './pages/ProductPage';
-import StatsPage from './pages/StatsPage';
+import CustomerPage from './pages/customers/CustomerPage';
+import OrderPage from './pages/orders/OrderPage';
+import ProductPage from './pages/products/ProductPage';
+import StatsPage from './pages/stats/StatsPage';
 import { hot } from 'react-hot-loader';
 import { Route } from "react-router-dom";
 
 import HeaderComp from './components/layout/Header';
 import FooterComp from './components/layout/Footer';
 
+import CustomerCreatePage from './pages/customers/Customer-Create-Page';
+
 const Routes = () => {
     return (
         <div>
             <HeaderComp />
-            <Route path='/' exact={true} component={ HomeComp } />
-            <Route path='/customers' exact={true} component={ CustomerPage } />
-            <Route path='/orders' exact={true} component={ OrderPage } />
-            <Route path='/products' exact={true} component={ ProductPage } />
-            <Route path='/stats' exact={true} component={ StatsPage } />
+            <Route path='/' exact component={ HomeComp } />
+            <Route path='/customers' exact component={ CustomerPage } />
+            <Route path='/customers/create' exact component={ CustomerCreatePage } />
+            <Route path='/orders' exact component={ OrderPage } />
+            <Route path='/products' exact component={ ProductPage } />
+            <Route path='/stats' exact component={ StatsPage } />
             <FooterComp />
         </div>
         
