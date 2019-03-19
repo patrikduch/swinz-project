058b61801f88712c0cc15abfc55af652e3fe3a8a
patrikduch <duchpatrik@icloud.com>
2019-03-19 17:17:40 +0100

    Basic unit tests structure

---
+++ Swinz.Tests/Services/ApiBase/ApiBaseTests.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ApiBaseTests.cs" website="Patrikduch.com">
+//     Copyright 2019 (c) Patrikduch.com
+// </copyright>
+// <author>Patrik Duch</author>
+//-----------------------------------------------------------------------
+
+namespace Swinz.Tests.Services.ApiBase
+{
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for ApiBase service
+    /// </summary>
+    public class ApiBaseTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ Swinz.Tests/Services/UserApi/UserApiTests.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UserApiTests.cs" website="Patrikduch.com">
+//     Copyright 2019 (c) Patrikduch.com
+// </copyright>
+// <author>Patrik Duch</author>
+//-----------------------------------------------------------------------
+
+namespace Swinz.Tests.Services.UserApi
+{
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for User REST API
+    /// </summary>
+    public class UserApiTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
