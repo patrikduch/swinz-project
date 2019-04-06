c3e5f4039587523aceaaa2a21d5ba84bc3f2fca8
patrikduch <duchpatrik@icloud.com>
2019-04-06 12:55:44 +0200

    Product database model extended

--- Services/OrderApi/Migrations/20190406105242_ProductTablePriceAttribute.Designer.cs
+++ Services/OrderApi/Migrations/20190406105242_ProductTablePriceAttribute.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrderApi.Contexts;
 
 namespace OrderApi.Migrations
 {
     [DbContext(typeof(ProductContext))]
-    partial class ProductContextModelSnapshot : ModelSnapshot
+    [Migration("20190406105242_ProductTablePriceAttribute")]
+    partial class ProductTablePriceAttribute
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
