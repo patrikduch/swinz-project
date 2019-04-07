4d69499c6a5c33ae798023ba5694fcc4e2986086
patrikduch <duchpatrik@icloud.com>
2019-04-07 10:16:34 +0200

    OrderApi database model

---
+++ Services/OrderApi/EntityConfigurations/OrderProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PersistenceLib.Domains.OrderApi;
+
+namespace OrderApi.EntityConfigurations
+{
+    public class OrderProductConfiguration : IEntityTypeConfiguration<OrderProduct>
+    {
+        public void Configure(EntityTypeBuilder<OrderProduct> builder)
+        {
+            builder.ToTable("OrderProduct");
+
+            // Composite key
+            builder.HasKey(sc => new { sc.OrderId, sc.ProductId });
+
+
+        }
+    }
+}
---
+++ Services/OrderApi/Migrations/20190407071841_OrderProductEntity.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrderApi.Migrations
+{
+    public partial class OrderProductEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderProduct",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProduct", x => new { x.OrderId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_OrderProduct_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderProduct_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderProduct_ProductId",
+                table: "OrderProduct",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderProduct");
+        }
+    }
+}
