5c4cb63e8df9fc1b3b12515d336bb39612e83510
patrikduch <duchpatrik@icloud.com>
2019-05-07 06:59:16 +0200

    Unit test optimalization

---
+++ Swinz.Tests/Tests/Services/StatsApi/StatsApiTests.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using PersistenceLib.Domains.OrderApi;
+using StatsApi.Contexts;
+using StatsApi.Repositories;
+
+namespace Swinz.Tests.Services.StatsApi
+{
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for Statistics REST API
+    /// </summary>
+    public class StatsApiTests
+    {
+        /// <summary>
+        /// Calculation of getting last year income
+        /// </summary>
+        [Fact]
+        public void GetLastYearIncome_ReturnsNumericValue()
+        {
+
+            // Arrange
+
+            var orderProducts = new List<OrderProduct>()
+            {
+                new OrderProduct
+                {
+                    Id = 1,
+                    OrderId = 1,
+                    ProductId = 1,
+                    Product = new Product
+                    {
+                        Id = 1,
+                        Name = "Product",
+                        Price = 256,
+                        IsDeleted = false,
+                    }
+                },
+
+                new OrderProduct
+                {
+                    Id = 2,
+                    OrderId = 2,
+                    ProductId = 2,
+                    Product = new Product
+                    {
+                        Id = 2,
+                        Name = "Product2",
+                        Price = 10,
+                        IsDeleted = false,
+                    }
+                }
+
+
+            };
+
+            var customers =
+                new List<Order>
+                {
+
+                    new Order()
+                    {
+                        Id = 1,
+                        CreationDate = System.DateTime.Now,
+                        OrderProducts = orderProducts
+                    }
+                }.AsQueryable();
+
+
+            var mockContext = new Mock<CustomerStatsContext>();
+            var repository = new CustomerStatsRepository(mockContext.Object);
+
+            // Act
+            var actual = repository.GetLatestIncome(customers);
+
+            // Assert
+            Assert.Equal(266, actual);
+
+        }
+
+        /// <summary>
+        /// Calculation of getting total number of sold products
+        /// </summary>
+        [Fact]
+        public void GetSoldCount_ReturnsNumericValue()
+        {
+
+            // Arrange
+
+            var orderProducts = new List<OrderProduct>()
+            {
+                new OrderProduct
+                {
+                    Id = 1,
+                    OrderId = 1,
+                    ProductId = 1,
+                    Product = new Product
+                    {
+                        Id = 1,
+                        Name = "Product",
+                        Price = 256,
+                        IsDeleted = false,
+                    }
+                },
+
+                new OrderProduct
+                {
+                    Id = 2,
+                    OrderId = 2,
+                    ProductId = 2,
+                    Product = new Product
+                    {
+                        Id = 2,
+                        Name = "Product2",
+                        Price = 10,
+                        IsDeleted = false,
+                    }
+                }
+
+
+            };
+
+            var customers =
+                new List<Order>
+                {
+
+                    new Order()
+                    {
+                        Id = 1,
+                        CreationDate = System.DateTime.Now,
+                        OrderProducts = orderProducts
+                    }
+                }.AsQueryable();
+
+
+            var mockContext = new Mock<CustomerStatsContext>();
+
+
+            // Act
+            var repository = new CustomerStatsRepository(mockContext.Object);
+
+            var actual = repository.GetSoldCount(customers);
+
+            // Assert
+
+            Assert.Equal(2, actual);
+
+        }
+
+    }
+}
